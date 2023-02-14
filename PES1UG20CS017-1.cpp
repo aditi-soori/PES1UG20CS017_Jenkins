224fcb2e11de6cb0bb3aeb331a38dfd937a2e41a
Aditi Soori <77949362+aditi-soori@users.noreply.github.com>
2023-02-14 16:20:57 +0530

    Create PES1UG20CS017-1.cpp

---
+++ PES1UG20CS017-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Pipeline successfully built and run\n";
+  cout << "PES1UG20CS017 ADITI SOORI\n";
+  return 0;
+}
